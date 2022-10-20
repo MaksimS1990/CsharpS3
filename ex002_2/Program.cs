6b43d4e5e70b74715231d9b884ccdc7f244ec206
MaksimS <sipatovgt@mail.ru>
2022-10-20 22:38:05 +0400

    ex002_2version

---
+++ ex002_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите координаты точки А (х,y)");
+int xa = int.Parse(Console.ReadLine()!);
+int ya = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки B (х,y)");
+int xb = int.Parse(Console.ReadLine()!);
+int yb = int.Parse(Console.ReadLine()!);
+
+double Length(int xa, int ya, int xb, int yb)
+{
+    double result = 0;
+    result =  Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2));
+    return result;
+}
+
+Console.WriteLine("Длина отрезка " + Length(xa, ya, xb, yb));
+
