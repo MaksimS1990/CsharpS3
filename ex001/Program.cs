3ff73dfca92107e82fb40a95cf8b8cf8ec5294b4
MaksimS <sipatovgt@mail.ru>
2022-10-20 22:24:39 +0400

    ex001

---
+++ ex001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+string GetRange(int quarter)
+{
+    if (quarter==1) return "x>0 & y>0";
+    if (quarter==2) return "x<0 & y>0";
+    if (quarter==3) return "x<0 & y<0";
+    if (quarter==4) return "x>0 & y<0";
+
+    return "unknown quarter";
+}
+
+Console.WriteLine("Enter the Quarter number:");
+int quarterNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(GetRange(quarterNumber));
